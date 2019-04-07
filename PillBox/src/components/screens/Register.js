00e16b14f733058786be8876cce3556583df2b95
Kyle Hardy <31456515+kdh022@users.noreply.github.com>
2019-04-07 17:00:05 -0500

    Update Register.js

--- PillBox/src/components/screens/Register.js
+++ PillBox/src/components/screens/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, TouchableOpacity, TextInput, Button, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Button, KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { RegisterForm } from '../container/RegisterForm'
 import config from '../../config';
 
@@ -12,15 +12,65 @@ class Register extends Component {
     render() {
         return (
             <KeyboardAvoidingView style={config.fullLayout}>
-            <TouchableOpacity>
-                <Text style={{fontSize: 30}}>Registration</Text>
-                <RegisterForm/>
-                <TouchableOpacity style={styles.buttons}
-                onPress={()=> this.register()}>
-                    <Text style={styles.registerText}>Register</Text>
-                </TouchableOpacity>
+                <Text style={{fontSize: 24}}>Registration</Text>
+				{/* <RegisterForm/> */}
+			<View>
+                <View 
+                    style={config.formLayout}>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        placeholder="Name"
+                        autoFocus={true}
+                        returnKeyType="next"
+                        onSubmitEditing={() => this.passwordInput.focus()}
+                    />
+                </View>
 
-            </TouchableOpacity>
+                <View 
+                    style={config.formLayout}> 
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        placeholder="Email"
+                        returnKeyType="next"
+                        onSubmitEditing={() => this.passwordInput.focus()}
+                    />                    
+                </View>
+
+                <View 
+                    style={config.formLayout}> 
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        placeholder="Username"
+                        returnKeyType="next"
+                        onSubmitEditing={() => this.passwordInput.focus()}
+                    />
+                </View>
+
+                <View 
+                    style={config.formLayout}> 
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        ref={(input) => this.passwordInput = input}
+                        autoCorrect={false}
+                        placeholder="Password"
+                        returnKeyType="go"
+                        secureTextEntry
+                    />
+                </View>
+	        </View>
+
+				<TouchableOpacity 
+                    style={styles.buttons}
+					onPress={()=> this.register()}>
+					<Text style={styles.registerText}>Submit</Text>
+				</TouchableOpacity>
             </KeyboardAvoidingView>
         )
     }
@@ -35,10 +85,17 @@ const styles = StyleSheet.create({
         margin: 5,
         padding: 10,
         backgroundColor: config.colors.darkGreen
-    },
-    loginText: {
+	},
+	registerText: {
         fontSize: 24,
         color: 'white',
     },
+	input: {
+        height: 40,
+        marginBottom: 10,
+        padding: 10,
+        fontSize: 20
+    }
 });
+
 export default Register;
